306fbb97babc0073fb2abce230542ae0262a5079
Alex Slepak <alex.slepak@lovevery.com>
2024-03-13 18:49:51 -0700

    add controller logic and tests

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,11 +1,25 @@
 class NodesController < ApplicationController
+  before_action :load_nodes, only: :common_ancestor
+
   def common_ancestor
-    render json: node.lowest_ancestor(other_node: node(param_name: :b)), status: :ok
+  render json: @node_a.lowest_ancestor(other_node: @node_b), status: :ok
   end
 
   private
 
-  def node(param_name: :a)
-    Node.find(params[param_name])
+  def load_nodes
+    @node_a =  load_node
+    if @node_a.nil?
+      render json: { error: "Node not found" }, status: 404 and return
+    end
+
+    @node_b = load_node(param_name: :b)
+    if @node_b.nil?
+      render json: { error: "Node not found" }, status: 404 and return
+    end
+  end
+
+  def load_node(param_name: :a)
+    Node.find_by(id: params[param_name])
   end
 end
